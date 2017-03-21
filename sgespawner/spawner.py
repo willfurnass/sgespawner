f12aaddac95d87ed2faea8d89b56dc7bae5e538f
Will Furnass <will@thearete.co.uk>
2017-03-21 20:42:16 +0000

    qsub jobs using instantiated jinja2 template

--- sgespawner/spawner.py
+++ sgespawner/spawner.py
@@ -1,10 +1,12 @@
-import sys
+#import os
+#import sys
 import time
-import subprocess
+from subprocess import Popen, PIPE, STDOUT, run
+#import tempfile
 
+import jinja2
 from tornado import gen
-
-from traitlets import Dict
+from traitlets import Dict, Unicode
 
 from jupyterhub.utils import random_port
 from jupyterhub.spawner import Spawner
@@ -16,7 +18,9 @@ __all__ = ['SGESpawner']
 class SGESpawner(Spawner):
 
     sge_env = Dict({}, config=True,
-                   help="Extra SGE environment variables to pass through")
+                   help="Extra SGE environment variables needed to submit a job ")
+    sge_template = Unicode('', config=True,
+                   help="Filename of Jinja 2 template for a SGE batch job script")
 
     def __init__(self, *args, **kwargs):
         super(SGESpawner, self).__init__(*args, **kwargs)
@@ -43,8 +47,8 @@ class SGESpawner(Spawner):
             The value of the column, or None if the job can not be found
         """
         qstat_columns = {'state': 4, 'host': 7}
-        ret = subprocess.run(self.cmd_prefix + ['qstat', '-t'],
-                             stdout=subprocess.PIPE, env=self.env)
+        ret = run(self.cmd_prefix + ['qstat', '-t'],
+                             stdout=PIPE, env=self.env)
 
         jobinfo = ret.stdout.decode('utf-8')
 
@@ -84,21 +88,41 @@ class SGESpawner(Spawner):
         """
         self.user.server.port = random_port()
 
+        #tmpdir = os.environ['TMPDIR'] if 'TMPDIR' in os.environ else '/tmp'
+        #file_descr, tmpfile = tempfile.mkstemp(dir=tmpdir, text=True)
+        #with open(tmpfile, 'w') as f:
+        #    f.write(jinja2.Template(self.sge_template).render(
+        #        working_dir='/home/{}'.format(self.user.name),
+        #        jh_args=self.get_args()))
+        with open(self.sge_template, 'r') as f:
+            batch_job_submission_script = jinja2.Template(f.read()).render(
+                working_dir='/home/{}'.format(self.user.name),
+                jh_args=' '.join(self.get_args()))
+        self.log.info("SGE: batch job sub script: '{}'".format(batch_job_submission_script))
         cmd = self.cmd_prefix.copy()
         cmd.extend(['qsub',
-                    '-b', 'y',
-                    '-j', 'y',
-                    '-N', 'jupyterhub',
-                    '-wd', '/home/{}'.format(self.user.name)])
-        cmd.extend([sys.executable, '-m', 'jupyterhub.singleuser'])
-        cmd.extend(self.get_args())
+                    '-v', 'JPY_API_TOKEN',
+                    #tmpfile
+                    ])
+        # NOT NEEDED ANY MORE
+        #            '-wd', '/home/{}'.format(self.user.name)])
+        #cmd.extend([sys.executable, '-m', 'jupyterhub.singleuser'])
+        #cmd.extend(self.get_args())
 
         self.log.info("SGE: CMD: {}".format(cmd))
 
         env = self.env.copy()
 
-        self.proc = subprocess.Popen(cmd, env=env, stdout=subprocess.PIPE)
-        r = self.proc.stdout.read().decode('utf-8')
+        #self.proc = Popen(cmd, stdin=batch_job_submission_script,
+        #                             env=env, stdout=PIPE)
+        #r = self.proc.stdout.read().decode('utf-8')
+        self.proc = Popen(cmd,
+                          stdout=PIPE,
+                          stdin=PIPE,
+                          stderr=STDOUT,
+                          env=env)
+        r = self.proc.communicate(
+            input=batch_job_submission_script.encode('utf-8'))[0].decode('utf-8')
         self.log.info("SGE: {}".format(r))
         jid = int(r.split('Your job ')[1].split()[0])
         self.jobid = jid
@@ -118,9 +142,9 @@ class SGESpawner(Spawner):
     @gen.coroutine
     def stop(self, now=False):
         if self.jobid:
-            ret = subprocess.Popen(self.cmd_prefix +
-                                   ['qdel', '{}'.format(self.jobid)],
-                                   env=self.env)
+            ret = Popen(self.cmd_prefix +
+                        ['qdel', '{}'.format(self.jobid)],
+                        env=self.env)
             self.log.info("SGE: {}".format(ret))
 
     @gen.coroutine
